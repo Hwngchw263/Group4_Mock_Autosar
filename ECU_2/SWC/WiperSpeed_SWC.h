48ad21586af3b1f512854094fd42f8ec21f5b81b
annhtuan <tuananhbka123@gmail.com>
2024-08-16 22:07:30 +0700

    fix linh tinh

--- ECU_2/SWC/WiperSpeed_SWC.h
+++ ECU_2/SWC/WiperSpeed_SWC.h
@@ -4,10 +4,10 @@
 #include <stdint.h>
 #include <Rte_DataHandleType.h>
 //value for speed mode
-#define LOW_SPEED = 50
-#define HIGH_SPEED = 75
-#define LOW_DUTY_CYCLE = 500
-#define HIGH_DUTY_CYCLE = 750
+#define LOW_SPEED  50
+#define HIGH_SPEED  75
+#define LOW_DUTY_CYCLE  500
+#define HIGH_DUTY_CYCLE  750
 
 
 FUNC(void,SprayFluid_CODE) Runnable_WiperSpeed(VAR(void,AUTOMATIC));
